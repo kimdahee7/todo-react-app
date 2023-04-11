bdf3a67bafb283c44b28b81054817f2a308525e9
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-04-11 11:26:24 +0900

    4/10 수업 완료

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import './App.css';
 import Todo from './Todo';
+import { Paper, List } from "@material-ui/core";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      item: {id:0, title: "Hello World 1", done: true},
+      items : [
+        {id:0, title: "Hello World 1", done: true},
+        {id:1, title: "Hello World 2", done: false},
+      ],
     };
   }
   render() {
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{margin: 16}}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id}/>
+          ))}
+        </List>
+      </Paper>
+    );
     return(
     <div className="App">
-      <Todo item={this.state.item}/>
+      {/* <Todo> 컴포넌트 여러 개 */}
+      {todoItems}
       </div>
     );
   }
