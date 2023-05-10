8759d223b4846a21c67da2f7c9a953551350d37a
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-05-10 13:16:51 +0900

    5/10 수업 완료

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -20,5 +20,25 @@ export function call(api, method, request) {
             }
             return json;
         })
+    )
+        .catch((error) => {
+            console.log(error.status);
+            if(error.status === 403) {
+                window.location.href = "/login";
+            }
+            return Promise.reject(error);
+        }
      );
+}
+
+export function signin(userDTO) {
+    return call("/auth/signin", "POST", userDTO)
+    .then((response) => {
+        //console.log("response: ", response);
+        //alert("로그인 토큰: ", + response.token)
+        if(response.token) {
+            localStorage.setItem("ACESS_TOKEN", response.token);
+            window.location.href = "/";
+        }
+    });
 }
